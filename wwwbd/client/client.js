422a9569c285ff320ae70326bb973ee9235bbc90
Quin <rqthames@yahoo.com>
2013-11-11 17:37:42 -0800

    --refactored so that query to google finance api only makes one call per render

--- wwwbd/client/client.js
+++ wwwbd/client/client.js
@@ -1,6 +1,7 @@
 Deps.autorun(function () {
     Meteor.subscribe("blog");
     Meteor.subscribe("holdings");
+    Meteor.subscribe("users");
 });
 
 Template.hello.greeting = function () {
@@ -20,25 +21,36 @@ Template.holds.holdings = function() {
 }
 
 Template.holds.rendered = function() {
-	Holdings.find().forEach(function(stock){
-		var url = "https://www.google.com/finance/info?infotype=infoquoteall&q=";
-		Meteor.call("getQuote", url+stock.ticker, function(error, results) {
-		    var resultsStr = results.content,
-		        quote = JSON.parse(resultsStr.substr(resultsStr.indexOf('//')+2).trim());
-            $("#" + stock.ticker)
-                .empty()
-                .append("<td>" + quote[0].name + "</td>")
-                .append("<td>" + quote[0].t + "</td>")
-                .append("<td>" + quote[0].l + "</td>")
-                .append("<td>" + quote[0].c + "</td>")
-                .append("<td>" + quote[0].cp + "</td>")
-                .append("<td>" + quote[0].hi + "</td>")
-                .append("<td>" + quote[0].lo + "</td>")
-                .append("<td>" + quote[0].hi52 + "</td>")
-                .append("<td>" + quote[0].lo52 + "</td>")
+  //var result = Holdings.aggregate({$group: {_id:"", tickers: {$push: "$ticker"}}}),
+  //    tickers = results[0].tickers;
+  var tickers = [],
+      url = "https://www.google.com/finance/info?infotype=infoquoteall&q=";
 
-		});
-	})
+  Holdings.find().forEach(function(hold) {
+      tickers.push(hold.ticker);
+  });
+  
+  if(tickers.length) {
+    	Meteor.call("getQuote", url+tickers.join(","), function(error, results) {
+    	    var resultsStr = results.content,
+    	        quotes = JSON.parse(resultsStr.substr(resultsStr.indexOf('//')+2).trim());
+          quotes.forEach(function(quote, index) {
+              $("#" + tickers[index])
+                  .empty()
+                  .append("<td>" + quote.name + "</td>")
+                  .append("<td>" + quote.t + "</td>")
+                  .append("<td>" + quote.l + "</td>")
+                  .append("<td>" + quote.c + "</td>")
+                  .append("<td>" + quote.cp + "</td>")
+                  .append("<td>" + quote.hi + "</td>")
+                  .append("<td>" + quote.lo + "</td>")
+                  .append("<td>" + quote.hi52 + "</td>")
+                  .append("<td>" + quote.lo52 + "</td>")
+          
+          })
+
+    	});
+  }
 }
   
 Template.says.posts = function() {
--- wwwbd/common/common.js
+++ wwwbd/common/common.js
@@ -1,2 +1,3 @@
 Blog = new Meteor.Collection('blog');
 Holdings = new Meteor.Collection('holdings');
+Users = new Meteor.Collection('users');
