6671136a76fd53a1c60c41f91a13ec89ffde687e
Quin <rqthames@yahoo.com>
2013-12-10 17:50:02 -0800

    --input links in posts are linkified but html input is interrpretted as text to prevent malicious attacks

--- wwwbd/client/client.js
+++ wwwbd/client/client.js
@@ -1,4 +1,21 @@
-var actions = ["buy", "sell", "say"];
+var actions = ["buy", "sell", "say"],
+    linkify = function (inputText) {
+        var replacedText, replacePattern1, replacePattern2, replacePattern3;
+
+        //URLs starting with http://, https://, or ftp://
+        replacePattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
+        replacedText = inputText.replace(replacePattern1, '<a href="$1" target="_blank">$1</a>');
+
+        //URLs starting with "www." (without // before it, or it'd re-link the ones done above).
+        replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
+        replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2" target="_blank">$2</a>');
+
+        //Change email addresses to mailto:: links.
+        replacePattern3 = /(([a-zA-Z0-9\-\_\.])+@[a-zA-Z\_]+?(\.[a-zA-Z]{2,6})+)/gim;
+        replacedText = replacedText.replace(replacePattern3, '<a href="mailto:$1">$1</a>');
+
+        return replacedText;
+    };
 
 Deps.autorun(function () {
     Meteor.subscribe("blog");
@@ -109,6 +126,13 @@ Template.hold.rendered = function() {
 }
 
 Template.post.rendered = function() {
+    $(".textContainer").each(function () {
+        if (!$(this).text().match(/[<>]/g)) {
+            $(this) .html(linkify($(this).text()));
+        }
+    });
+
+
     $(".deletePostButton")
         .unbind()
         .click(function() {
